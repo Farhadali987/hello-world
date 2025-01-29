73cd632cbb88b4f37408cc6db7dc638cbf4c5586
Farhad gul <164610570+Farhadali987@users.noreply.github.com>
2025-01-29 11:42:50 -0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Hello() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1 style={{ color: 'blue' }}>Hello, World!</h1>
+    </div>
+  );
+}
